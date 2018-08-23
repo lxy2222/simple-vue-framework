4b7a12dc23034c8a0873a0578734c498217d29a2
liangxiaoyu <liangxiaoyu@100tal.com>
2018-08-23 18:35:57 +0800

    修改webpack配置

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-Vue.use(ElementUI)
+import {
+    Button
+} from 'element-ui'
+
+Vue.use(Button)
 new Vue({
     el: '#app',
     router,
-    components: { App },
+    components: {
+        App
+    },
     template: '<App/>'
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,53 @@
 const webpack = require('webpack')
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'js/bundle.js'
-    }
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].js'
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: './index.html',
+      template: './src/index.html',
+      hash: false
+    }),
+    // make sure to include the plugin!
+    new VueLoaderPlugin()
+  ],
+  resolve: {
+    extensions: ['.js', '.vue', '.scss', '.css'], //后缀名自动补全
+    // alias: {
+    //   'vue$': 'vue/public/vue.esm.js'
+    // }
+  },
+  module: {
+    rules: [{
+      test: /\.vue$/,
+      loader: 'vue-loader'
+    }, {
+      test: /\.js$/,
+      loader: 'babel-loader'
+    }, {
+      test: /\.css$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader'
+      }]
+    }, {
+      test: /\.(png|jpg)$/,
+      exclude: /^node_modules$/,
+      loader: 'url-loader?limit=8192&name=images/[hash:8].[name].[ext]'
+    }, {
+      test: /\.(eot|woff|svg|ttf|woff2|gif|appcache)(\?|$)/,
+      exclude: /^node_modules$/,
+      loader: 'file-loader?name=[name].[ext]'
+    }]
+  }
 }
